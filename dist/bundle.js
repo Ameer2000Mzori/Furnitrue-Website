b76c9f8e66b81d533816a5b57824ee4c59c45271
Ameer <ameerapex496@gmail.com>
2023-12-01 06:14:11 +0100

    fixed styling bugs / merged navbar to main

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/script.js":
-/*!***********************!*\
-  !*** ./src/script.js ***!
-  \***********************/
-/***/ (() => {
-
-eval("console.log(\"hallo world\");\n\n//# sourceURL=webpack://scroll-animation/./src/script.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/script.js"]();
 /******/ 	
 /******/ })()
 ;
